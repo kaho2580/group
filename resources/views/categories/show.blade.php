1a046b277d8c216a67dc64fee605e0e1a824ef69
Hiroki1278 <c14h14n3nao3s@gmail.com>
2022-08-07 17:00:53 +0900

    [modified]コンフリクトの修正

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -2,17 +2,15 @@
 @section('content')
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-
+    <title>レビュー詳細</title>
     </head>
     <body style="background-color:white; font-family:Hannotate SC; color:#3f3f3f; text-align:center;">
         <h1 style="color:#e2914a; font-size:50px;">{{$review->title}}</h1>
          <h2>{{$review->body}}</h2>
          <h2>{{$review->visited_at}}</h2>
-         <h3>{{$review->updated_at}}</h3>
-          <a href="/categories/reviews/{{ $review->id }}" style="color:#7a54a8;">編集</a>
-          
-          <a href="/categories/{{$review->category->id}}">戻る</a>
-
+    </div>
+    <a href="/categories/reviews/edit/{{$review->id}}">編集</a>
+    <a href="/">戻る</a>
     </body>
     </html>
     @endsection
