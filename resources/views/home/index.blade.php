8f5867ded1c4061aa8511b2c16a35a83eefeb67d
Ryudaiver1 <uec20190@gmail.com>
2022-08-07 11:07:06 +0900

    変更

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,39 +7,11 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>レバテックチーム開発</h1>
-        <h2>投稿一覧ページ</h2>
-        <div class='posts'>
-            @foreach($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        タイトル：<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                    <p class='body'>本文：{{ $post->body}}</p>
-                    <p>カテゴリー:<a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a></p>
-                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}"  method="post" style="display:inline">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onClick="deletePost({{$post->id}});">削除</button> {{--script内に定義したdeletePostを使用している--}}
-                    </form>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
-        <div>
-            [<a href='/posts/create'>新規作成</a>]
-        </div>
+        <h1>home</h1>
+        <h2>お好きなタグを選んでください！</h2>
+        <button id='btn'>こってり</button>
+        <button id='btn'>あっさり</button>
+        <button id='btn'>高級</button>
+        <button id='btn'>格安</button>
     </body>
-    <script>
-        function deletePost(post_id) {
-            form = document.getElementById('form_' + post_id);  //各投稿ごとのdeleteのformを取得
-            is_submit = confirm('本当に削除してもよろしいですか？'); //はいの場合true,いいえの場合falseをis_submitに格納
-            
-            if(is_submit) {  //is_submitがtrueの場合のみ、{}の中の処理が行われる
-                form.submit();  //deleteするformをsubmitする（投稿を削除している）
-            }
-        }
-    </script>
 </html>
