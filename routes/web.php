5d136fd75103f1c1aa0f5282e1d68af89540a8a0
Ryudaiver1 <uec20190@gmail.com>
2022-08-07 18:01:07 +0900

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,12 @@ Route::put('/posts/{post}', 'PostController@update'); //編集操作
 Route::get('/posts/{post}', 'PostController@show'); //投稿詳細画面
 Route::post('/posts', 'PostController@store'); //投稿保存操作
 Route::delete('/posts/{post}', 'PostController@delete'); //投稿削除
+Route::get('/categories/reviews/{ review }', 'CategoryController@show'); //詳細表示
+Route::get('/categories/{category}', 'CategoryController@category'); //カテゴリー一覧画面
 
-Route::get('/categories/kotteri', 'CategoryController@assari'); //こってり
-Route::get('/categories/assari', 'CategoryController@assari'); //あっさり
-Route::get('/categories/takai', 'CategoryController@assari'); //高い
-Route::get('/categories/yasui', 'CategoryController@yasui'); //安い
+//検索機能
+Route::get('/search','SearchController@index');
+Route::post('/search/word','SearchController@searchWord');
 
 
 
